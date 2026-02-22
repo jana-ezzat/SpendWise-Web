376076517a802f649c1db052e8531648b1403b56
jana-ezzat <janaezzat362@gmail.com>
2026-02-22 11:47:38 +0200

    Add sidebar component files and the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { SidebarProvider } from "./components/ui/sidebar";
+import AppSidebar from "./components/layout/Sidebar";
+import Budget from "./pages/Budget";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <SidebarProvider>
+        <div className="flex min-h-screen w-full">
+          <AppSidebar />
+          <main className="flex-1 bg-rose-50">
+            <Routes>
+              <Route path="/budget" element={<Budget />} />
+            </Routes>
+          </main>
+        </div>
+      </SidebarProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
